777ca784c2de301c1ad36eb7261155d4d68b0b5e
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-10-03 20:46:36 -0300

    Primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Tryunfo</h1>
-      </div>
+      <>
+        <div className="header">
+          <h1>Tryunfo</h1>
+        </div>
+        <Form />
+      </>
     );
   }
-} 
+}
 
 export default App;
